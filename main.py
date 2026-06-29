72bd2003437f092c9a4b0180aa24cc9cde7b21d5
zzak249-beep <zzak249@gmail.com>
2026-06-29 21:06:20 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-import runpy
-import sys
-import os
-
-sys.path.insert(0, "/app")
-os.chdir("/app")
-runpy.run_path("/app/scanner.py", run_name="__main__")
+import os, sys
+os.execv(sys.executable, [sys.executable, "/app/scanner.py"])
